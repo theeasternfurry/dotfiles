59e6938b1602746c099a636378bee50673ec71f2
theeasternfurry <theeasternfurry@outlook.com>
2026-02-12 12:07:56 +0700

    wezterm: update color scheme and auto set theme based on time

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,4 +6,12 @@ config.default_prog = { 'fish' }
 config.font_size = 12
 config.font = wezterm.font('VictorMono Nerd Font')
 
+local hour = tonumber(wezterm.time.now():format("%H"))
+
+if hour >= 6 and hour < 18 then
+    config.color_scheme = 'Rosé Pine Dawn (Gogh)'
+else
+    config.color_scheme = 'Rosé Pine Moon (Gogh)'
+end
+
 return config

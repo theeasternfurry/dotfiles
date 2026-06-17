f0bddff81373f9ffb5e922e45535a5b790688a99
theeasternfurry <theeasternfurry@outlook.com>
2026-06-17 08:59:47 +0700

    wezterm: update config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,12 +6,6 @@ config.default_prog = { 'fish' }
 config.font_size = 12
 config.font = wezterm.font('VictorMono Nerd Font')
 
-local hour = tonumber(wezterm.time.now():format("%H"))
-
-if hour >= 6 and hour < 18 then
-    config.color_scheme = 'Rosé Pine Dawn (Gogh)'
-else
-    config.color_scheme = 'Rosé Pine Moon (Gogh)'
-end
+config.color_scheme = "Noctalia"
 
 return config

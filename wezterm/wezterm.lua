bc212ce52de0146ead93c601326da862f9f42f9b
theeasternfurry <theeasternfurry@outlook.com>
2026-06-26 14:27:35 +0700

    wezterm: config tab

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,8 +3,9 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.default_prog = { 'fish' }
-config.font_size = 12
+config.font_size = 11.5
 config.font = wezterm.font('VictorMono Nerd Font')
+config.hide_tab_bar_if_only_one_tab = true
 
 config.color_scheme = "Noctalia"
 

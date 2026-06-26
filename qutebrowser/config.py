8eeab4c6005710b9ece618a22f4711faeed5362b
theeasternfurry <theeasternfurry@outlook.com>
2026-06-26 16:25:20 +0700

    qutebrowser: add file picker zenity

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,14 +1,21 @@
 config.load_autoconfig(False)
 
-
-c.content.headers.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
-c.content.javascript.enabled = True
-
-c.tabs.position = "left"
-c.tabs.show = 'switching'
-
 c.qt.args = [
     'disable-gpu-compositing',
     'disable-software-rasterizer',
     'disable-features=CalculateNativeWinOcclusion'
 ]
+
+c.content.headers.user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+c.content.javascript.enabled = True
+
+# File selection
+c.fileselect.handler = 'external'
+c.fileselect.single_file.command = ['zenity', '--file-selection', '--title=Select File']
+c.fileselect.multiple_files.command = ['zenity', '--file-selection', '--multiple', '--title=Select Files']
+c.fileselect.folder.command = ['zenity', '--file-selection', '--directory', '--title=Select Folder']
+
+c.tabs.position = "left"
+c.tabs.show = 'switching'
+
+
